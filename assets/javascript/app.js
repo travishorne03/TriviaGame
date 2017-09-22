2be3673bc4919c9828c9ad49f9cc593b48a56384
T Horne <travis.horne03@gmail.com>
2017-09-22 17:55:27 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,11 +22,7 @@ $(document).on('keypress', function(event) {
 
 $(document).ready(function() {
 
-    //BUTTON TO START AND RESET THE GAME
-    $("startBtn").on("click".function() {
-        gameReset();
-
-    });
+  
 
     //START TIMER
     function startTimer() {
@@ -35,7 +31,7 @@ $(document).ready(function() {
         countdownTimer = setInterval(function() {
             $("#timer").html(j + " seconds remaining");
             q = q - 1;
-            if (j < 0) {
+            if (q < 0) {
                 clearInterval(countdownTimer);
                 $("#triviaQuestion").html("Your time is up!! The answer was: " + answerCode + ".");
                 qUnanswered++;
@@ -82,6 +78,71 @@ $(document).ready(function() {
         aAnswer: 1,
         iamge: "troy.gif",
     };
+
+
+    // function to reset variables on start/restart of game.    
+    function gameReset() {
+
+    }
+
+    // 30 second timer function. Displays remaining seconds (j) at #timer. Keeps track of unanswered question count
+    // (those without a click within 30 seconds).
+    function startTimer() {
+        $("#timer").html(" ");
+        j = 30;
+        countdownTimer = setInterval(function() {
+            $("#timer").html(j + " seconds remaining");
+            j = j - 1;
+            if (j < 0) {
+                clearInterval(countdownTimer);
+                $("#triviaQuestion").html("Time's up!!! The answer was: "+answerText+".");
+                qUnanswered++;
+                i++;
+                showAnswer();
+            }
+        }, 1000);
+    }
+
+    //RESET TIMER..NEXT QUESTION
+    function nextQuestion() {
+        startTimer(); 
+        nextQuestion();
+    }
+
+    // START QUESTION AND TIMER FUNCTION
+    function nextQuestion() {
+
+       
+
+    }
+
+    // SHOW ANSWER FUNCTION
+    function showAnswer() {
+
+        
+
+    }
+
+    // LIST GROUP AND SHOW IMAGES
+    $(".list-group-item").on("click", function() {
+        clearInterval(countdownTimer);
+ 
+
+        i++;
+        showAnswer();
+    })
+
+    // SHOW GAME RESULTES
+    function showResults() {
+
+       
+
+    }
+
+    // START GAME
+    $("#startBtn").on("click", function() {
+        resetGame();
+    });
 
 
     //CLOSING THE GAME FUNCTION
