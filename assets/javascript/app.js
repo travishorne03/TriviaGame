0f4bfe5c51e5428180446a7706ff4748d0bec725
T Horne <travis.horne03@gmail.com>
2017-09-25 19:35:01 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,35 +48,35 @@ $(document).ready(function() {
         question: "What jersey number did Deion wear while playing football in the NFL?"
         option: ["5", "21", "99", "54"],
         aAnswer: 1,
-        iamge: "deion.gif",
+        image: "deion.gif",
     };
 
     question[1] = {
         question: "How many Super Bowls did Troy Aikman win?"
         option: ["1", "6", "3", "10"],
         aAnswer: 2,
-        iamge: "troy.gif",
+        image: "troy.gif",
     };
 
     question[2] = {
         question: "What high school did Kobe Bryant attend?"
         option: ["Anson High", "Dudley High", "East Texas High", "Lower Merion High"],
         aAnswer: 3,
-        iamge: "troy.gif",
+        image: "troy.gif",
     };
 
     question[3] = {
         question: "Where did Michael Jordan attend college?"
         option: ["UNC-Chapel Hill", "Duke", "Wake Forest", "Yale"],
         aAnswer: 0,
-        iamge: "troy.gif",
+        image: "troy.gif",
     };
 
     question[4] = {
         question: "What MLB team did Derek Jeter Play for?"
         option: ["Cubs", "Yankees", "Red Sox", "Marlins"],
         aAnswer: 1,
-        iamge: "troy.gif",
+        image: "troy.gif",
     };
 
 
