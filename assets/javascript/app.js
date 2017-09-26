6533028342688a72614bea9cb0c00899b2ba6596
T Horne <travis.horne03@gmail.com>
2017-09-25 20:18:38 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,44 +7,17 @@ var qImage = "";
 var qCorrect = 0;
 var qIncorrect = 0;
 var qUnanswered = 0;
-var i = 0;
-var q = 0;
+var questionIx = 0;
+var questionQx = 0;
 var countdownTimer = 0;
 
-//ON CLICK BUTTON TO START  THE GAME
-$(document).on('keypress', function(event) {
-    if (event.keyCode === 13) {
-        // Enter key was pressed
-    }
-})
 
-//console.log(event.keyCode)
+$( document ).ready(function() {
 
-$(document).ready(function() {
+    //initialize variables  
+    var question = [];
 
-  
-
-    //START TIMER
-    function startTimer() {
-        $("#timer").html(" ");
-        q = 30;
-        countdownTimer = setInterval(function() {
-            $("#timer").html(j + " seconds remaining");
-            q = q - 1;
-            if (q < 0) {
-                clearInterval(countdownTimer);
-                $("#triviaQuestion").html("Your time is up!! The answer was: " + answerCode + ".");
-                qUnanswered++;
-                i++;
-                showAnswer();
-            }
-        }, 1000);
-    }
-
-
-
-
-    question[0] = {
+question[0] = {
         question: "What jersey number did Deion wear while playing football in the NFL?"
         option: ["5", "21", "99", "54"],
         aAnswer: 1,
@@ -80,66 +53,13 @@ $(document).ready(function() {
     };
 
 
-    // function to reset variables on start/restart of game.    
-    function gameReset() {
-
-    }
-
-    // 30 second timer function. Displays remaining seconds (j) at #timer. Keeps track of unanswered question count
-    // (those without a click within 30 seconds).
-    function startTimer() {
-        $("#timer").html(" ");
-        q = 30;
-        countdownTimer = setInterval(function() {
-            $("#timer").html(j + "seconds remaining");
-            q = q - 1;
-            if (q < 0) {
-                clearInterval(countdownTimer);
-                $("#triviaQuestion").html("Your time is up!! The answer was: "+answerText+".");
-                qUnanswered++;
-                i++;
-                showAnswer();
-            }
-        }, 1000);
-    }
-
-    //RESET TIMER..NEXT QUESTION
-    function nextQuestion() {
-        startTimer(); 
-        nextQuestion();
-    }
-
-    // START QUESTION AND TIMER FUNCTION
-    function nextQuestion() {
-
-       
-
-    }
-
-    // SHOW ANSWER FUNCTION
-    function showAnswer() {
-
-        
-
-    }
-
-    // LIST GROUP AND SHOW IMAGES
-    $(".list-group-item").on("click", function() {
-        clearInterval(countdownTimer);
-        i++;
-        showAnswer();
-    })
-
-    // SHOW GAME RESULTS
-    function showResults() {
-
-     }
-
-    // START GAME
-    $("#startBtn").on("click", function() {
-        resetGame();
-    });
 
 
-    //CLOSING THE GAME FUNCTION
-});
+var gameRunning = false
+
+if(!gameRunning) { ()}
+
+
+
+
+
