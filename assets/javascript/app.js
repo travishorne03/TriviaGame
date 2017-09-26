8cce61c9fb67ae8b6bcd3939eddcaf69a67ab2f3
T Horne <travis.horne03@gmail.com>
2017-09-25 20:24:23 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var qCorrect = 0;
 var qIncorrect = 0;
 var qUnanswered = 0;
 var questionIx = 0;
-var questionQx = 0;
+var questionJx = 0;
 var countdownTimer = 0;
 
 
@@ -54,10 +54,73 @@ question[0] = {
 
 
 
+// function to reset variables on start/restart of game.    
+    function gameReset() {
 
-var gameRunning = false
+    }
 
-if(!gameRunning) { ()}
+    // 30 second timer function. Displays remaining seconds (j) at #timer. Keeps track of unanswered question count
+    // (those without a click within 30 seconds).
+    function startTimer() {
+        $("#timer").html(" ");
+        questionJx = 30;
+        countdownTimer = setInterval(function() {
+            $("#timer").html(j + " seconds remaining");
+            questionJx = questionJx - 1;
+            if (questionJx < 0) {
+                clearInterval(countdownTimer);
+                $("#triviaQuestion").html("Sorry ... Time's up! The answer was: "+answerText+".");
+                qUnanswered++;
+                i++;
+                showAnswer();
+            }
+        }, 1000);
+    }
+
+    // function to move to next question & reset timer
+    function nextQuestion() {
+        startTimer(); 
+        resetQuestion();
+    }
+
+    // replace the html with the questions and possible answers. Populate variables with correct answer id, text & image.
+    function resetQuestion() {
+
+    
+
+    }
+
+    // hide the question div, update image and show image div. Wait 5 seconds then move to next question (nextQuestion()).
+    // If last question, call results function (showResults()).
+    function showAnswer() {
+
+        // show if they answered correctly or not and show image
+
+    }
+
+    // listen for click on "list-group". Check if answer is correct and update #triviaQuestion. Call showAnswer() to display image.
+    $(".list-group-item").on("click", function() {
+        clearInterval(countdownTimer);
+
+        // get clicked value 
+
+        i++;
+        showAnswer();
+    })
+
+    // Function to show game results. Update results div with stats and show() them. Hide() other irrelevant divs. Change text on start button and show().
+    function showResults() {
+
+        // at the end of the game, display total correct and incorrect
+
+    }
+
+    // button to start game
+    $("#startBtn").on("click", function() {
+        gameReset();
+    });
+
+});
 
 
 
