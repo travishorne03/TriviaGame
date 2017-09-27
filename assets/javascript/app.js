1bc642e18947bbd5138021abba908ff1913df1b2
T Horne <travis.horne03@gmail.com>
2017-09-27 15:25:38 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,46 +7,45 @@ var qImage = "";
 var qCorrect = 0;
 var qIncorrect = 0;
 var qUnanswered = 0;
-var questionIx = 0;
-var questionJx = 0;
+var i = 0;
+var j = 0;
 var countdownTimer = 0;
 var currentQuestion = 0;
-
 
 $( document ).ready(function() {
 
     //initialize variables  
-    var question = [];
+    var questions = [];
 
-question[0] = {
+questions[0] = {
         questions: "What jersey number did Deion Snaders wear while playing football in the NFL?"
         options: ["5", "21", "99", "54"],
         aAnswer: 1,
         image: "deion.gif",
     };
 
-    question[1] = {
+    questions[1] = {
         questions: "How many Super Bowls did Troy Aikman win?"
         options: ["1", "6", "3", "10"],
         aAnswer: 2,
         image: "troy.gif",
     };
 
-    question[2] = {
+    questions[2] = {
         questions: "What high school did Kobe Bryant attend?"
         options: ["Anson High", "Dudley High", "East Texas High", "Lower Merion High"],
         aAnswer: 3,
         image: "troy.gif",
     };
 
-    question[3] = {
+    questions[3] = {
         questions: "Where did Michael Jordan attend college?"
         options: ["UNC-Chapel Hill", "Duke", "Wake Forest", "Yale"],
         aAnswer: 0,
         image: "troy.gif",
     };
 
-    question[4] = {
+    questions[4] = {
         questions: "What MLB team did Derek Jeter Play for?"
         options: ["Cubs", "Yankees", "Red Sox", "Marlins"],
         aAnswer: 1,
@@ -54,8 +53,6 @@ question[0] = {
     };
 
 
-
-// function to reset variables on start/restart of game.    
     function gameReset() {
 
     }
@@ -63,14 +60,15 @@ question[0] = {
     // 30 second timer function. Displays remaining seconds (j) at #timer. Keeps track of unanswered question count
     // (those without a click within 30 seconds).
     function startTimer() {
+        console.log("startTimer");
         $("#timer").html(" ");
-        questionJx = 30;
+        j = 30;
         countdownTimer = setInterval(function() {
             $("#timer").html(j + " seconds remaining");
-            questionJx = questionJx - 1;
-            if (questionJx < 0) {
+            j = j - 1;
+            if (j < 0) {
                 clearInterval(countdownTimer);
-                $("#triviaQuestion").html("Sorry ... Time's up! The answer was: "+answerText+".");
+                $("#triviaQuestion").html("Time's up!!! The answer was: "+answerText+".");
                 qUnanswered++;
                 i++;
                 showAnswer();
@@ -79,20 +77,15 @@ question[0] = {
     }
 
     // function to move to next question & reset timer
-    function nextQuestion(answersDisplay) {
+    function nextQuestion() {
         startTimer(); 
         resetQuestion();
-            for (var i = 0; i < currentQuestion.option.length; i++) {
-                var newTag = $("<li></li>");
-                    var newAnchor = $("<a href='#' class='answer-option'>" + currentQuestion.options[i] + "</a>");
-                        $("#answer-display").append(newTag).append(newAnchor)
-            }
     }
 
     // replace the html with the questions and possible answers. Populate variables with correct answer id, text & image.
     function resetQuestion() {
 
-    
+        // jquery to update HTML questions & answers
 
     }
 
@@ -105,10 +98,8 @@ question[0] = {
     }
 
     // listen for click on "list-group". Check if answer is correct and update #triviaQuestion. Call showAnswer() to display image.
-    $("#answersDisplay").on("click", ".answer-options", function() {
-
-
-})
+    $(".list-group-item").on("click", function() {
+        clearInterval(countdownTimer);
 
         // get clicked value 
 
@@ -127,7 +118,7 @@ question[0] = {
     $("#startBtn").on("click", function() {
         gameReset();
     });
-
+startTimer();
 });
 
 
