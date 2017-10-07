55813b6f47294addac0abc060d73ea0cb7a1c169
T Horne <travis.horne03@gmail.com>
2017-10-06 21:15:07 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ var currentQuestion = 0;
 
 $(document).ready(function() {
 
-    //initialize variables  
+    //initialize variables
     var questions = [];
 
     questions[0] = {
@@ -80,12 +80,28 @@ $(document).ready(function() {
 
     // function to move to next question & reset timer
     function nextQuestion() {
-        startTimer();
         resetQuestion();
+        startTimer();
     }
 
     // replace the html with the questions and possible answers. Populate variables with correct answer id, text & image.
     function resetQuestion() {
+
+        //*** If we're out of questions, show results
+        if (i === questions.length) {
+
+          //*** The "return" keeps the rest of the resetQuestion function from running
+          return showResults();
+
+        }
+
+        //*** Set correctAnswer and answerText from questions
+        correctAnswer = questions[i].aAnswer;
+        answerText = questions[i].options[correctAnswer];
+
+        //*** Clears the background of all of the answers (the stylesheet takes over again)
+        $(".list-group-item").css({ 'background-color': '' });
+
         $("#trivia-question").html(questions[i].questions);
         $("#answer0").html(questions[i].options[0]);
         $("#answer1").html(questions[i].options[1]);
@@ -102,33 +118,50 @@ $(document).ready(function() {
 
         // show if they answered correctly or not and show image
 
+        //*** Sets the correct answer's background to bright green
+        $("#answer" + correctAnswer).css({ 'background-color': '#00ff00' });
+
+        //*** Wait 3 seconds (3000 milliseconds) before moving to the next question
+        setTimeout(function() {
+          resetQuestion();
+          startTimer();
+        }, 3000);
+
     }
 
     // listen for click on "list-group". Check if answer is correct and update #triviaQuestion. Call showAnswer() to display image.
     $(".list-group-item").on("click", function() {
-        console.log('click event triggered')
+        console.log('click event triggered');
         clearInterval(countdownTimer);
 
-    clickAnswer = $(this).attr("value"); 
-        if (clickAnswer == correctAnswer) { 
+        //*** The list-group-items don't have a "value" attribute
+        //*** I added "data-value" in the HTML. Need to parseInt to compare
+        clickAnswer = parseInt( $(this).attr("data-value") );
+
+        if (clickAnswer === correctAnswer) { //*** Use === instead of == (== can cause problems sometimes)
             $("#trivia-question").html("Correct Answer")
             qCorrect++;
         } else {
-            $("#trivia-question").html("Sorry, that's incorrect. The answer was: "+answerText+".")
+            $("#trivia-question").html("Sorry, that's incorrect. The answer was: " + answerText + ".")
             qIncorrect++;
         }
 
-        // get clicked value 
-
         i++;
         showAnswer();
-        resetQuestion();
+
     })
 
     // Function to show game results. Update results div with stats and show() them. Hide() other irrelevant divs. Change text on start button and show().
     function showResults() {
 
         // at the end of the game, display total correct and incorrect
+
+        //*** Hide the questions and answers
+        $("#answersDisplay").hide();
+        $("#trivia-question").hide();
+
+        $("#results").html("<span>RIGHT: " + qCorrect + ", WRONG: " + qIncorrect + "</span>");
+        $("#results").show();
 
     }
 
@@ -140,6 +173,3 @@ $(document).ready(function() {
     nextQuestion();
 
 });
-
-
-
